1842a47cd9a85664ad10178a1c392bbb34c27b03
matheus <matheustectj@gmail.com>
2024-09-23 11:07:24 -0300

    exercicio de poo classe pessoa

---
+++ PessoaClass/ex.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+class Pessoa {
+private:      
+    std::string nome;   // Nome da pessoa
+    int idade;         // Idade da pessoa
+    float altura;      // Altura da pessoa
+
+public:
+    // Construtor para inicializar os atributos
+    Pessoa(std::string n, int i, float a) : nome(n), idade(i), altura(a) {}
+
+    // Método para exibir os dados da pessoa
+    void exibirDados() const {
+        std::cout << "Nome: " << nome << ", Idade: " << idade << ", Altura: " << altura << "m" << std::endl;
+    }
+
+    // Métodos para acessar os atributos (getters)
+    std::string getNome() const { return nome; }
+    int getIdade() const { return idade; }
+    float getAltura() const { return altura; }
+
+    void setNome(const std::string& n) { nome = n; }
+    void setIdade(int i) { idade = i; }
+    void setAltura(float a) { altura = a; }
+};
+
+int main() {
+    // Criando um objeto da classe Pessoa
+    Pessoa pessoa1("João", 30, 1.75);
+    
+    float altura = pessoa1.getAltura();
+    std::cout << altura << std::endl;
+    // Exibindo os dados da pessoa
+    pessoa1.setIdade(31);
+
+    pessoa1.setNome("Laercio");
+
+    pessoa1.exibirDados();
+
+    return 0;
+}
